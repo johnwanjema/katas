d4cfc993fde2c5dc43a709184fad79b14c010e87
wanjema <jonwanjema@gmail.com>
2021-09-08 16:08:02 +0300

    add twice as old function

---
+++ javascript/ TwiceAsOld.js
@@ -0,0 +1,5 @@
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - (sonYearsOld * 2));
+}
