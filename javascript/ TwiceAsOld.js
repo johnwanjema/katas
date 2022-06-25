73f58441c0349fde246e60386984fb7ebef3bb42
John Wanjema <jonwanjema@gmail.com>
2022-06-24 18:44:25 -0700

    refactor to allow redability

--- javascript/ TwiceAsOld.js
+++ javascript/ TwiceAsOld.js
@@ -1,4 +1,5 @@
-// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+// Сalculate how many years ago the 
+// father was twice as old as his son (or in how many years he will be twice as old).
 
 function twiceAsOld(dadYearsOld, sonYearsOld) {
     return Math.abs(dadYearsOld - (sonYearsOld * 2));
